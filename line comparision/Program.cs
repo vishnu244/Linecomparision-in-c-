eab7d852e648313312ea55182d8256be824049d3
vishnu244 <vishnunali244@gmail.com>
2022-04-21 18:13:32 +0530

    creating a line

--- line comparision/Program.cs
+++ line comparision/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using line_comparision;
+
 Console.WriteLine("Welcome to Line Comparison!");
+
+
+while (true)
+{
+
+    Console.WriteLine("Choose the Program to be executed :\n1)createline");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            creatingline line = new();
+            line.Line();
+            break;
+    }
+}
---
+++ line comparision/creatingline.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace line_comparision
+{
+    public class creatingline
+    {
+        public void Line()
+        {
+            Console.WriteLine("Please Enter x1 and y1 coordinates : ");
+            var x1 = Convert.ToDouble(Console.ReadLine());
+            var y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Enter x2 and y2 coordinates : ");
+            var x2 = Convert.ToDouble(Console.ReadLine());
+            var y2 = Convert.ToDouble(Console.ReadLine());
+            var finalResult = Distance(x1, x2, y1, y2);
+            Console.WriteLine("Distance between {0},{1} and {2},{3} is {4:F}", x1, y1, x2, y2, finalResult);
+
+            static double Distance(double x1, double x2, double y1, double y2)
+            {
+                var temp1 = Math.Pow((x2 - x1), 2);
+                var temp2 = Math.Pow((y2 - y1), 2);
+                var result = Math.Sqrt(temp1 + temp2);
+                return result;
+            }
+        }
+    }
+}
